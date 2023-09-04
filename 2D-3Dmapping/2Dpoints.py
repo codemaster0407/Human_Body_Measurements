e947928a53df1756f43aa7cde493131ee649e260
“codemaster0407” <schaitanya0407@gmail.com>
2023-09-04 22:57:21 +0530

    code_changed

--- 2D-3Dmapping/2Dpoints.py
+++ 2D-3Dmapping/2Dpoints.py
@@ -1,22 +1,24 @@
 import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
-
-def list_files():
-    import os
-
-    folder_path = '/path/to/folder'  # Replace with the path to your folder
-
-    # Get all file names in the folder
-    file_names = os.listdir(folder_path)
-
-    # Print the file names
-    for file_name in file_names:
-        print(file_name)
+import os 
+import pandas as pd
 
 
 
 
+
+def find_y_min_max(coordinates):
+    y_min = 0
+    y_max = 0
+    
+
+    
+    y_min = min(coordinates[5][1], coordinates[2][1])
+    y_max = max(coordinates[32][1], coordinates[31][1])
+    # print(coordinates[32][1], coordinates[31][1]) 
+    # print(y_max, y_min)
+    return y_min, y_max
 
 
 
@@ -68,24 +70,40 @@ def normalized_coordinates(path):
 
 
 
-
+    # print(image.shape)
+    x_min = 0
+    y_min = 0
+    
+    x_max = image.shape[1]
+    y_max = image.shape[0]
 
     x_leftmost = int(coordinates[17][0] )
     x_rightmost = int(coordinates[18][0]) 
+    
+    #For finding the leftmost and right most y's
+    
+    y_leftmost, y_rightmost = find_y_min_max(coordinates)
 
-    y_leftmost = int(coordinates[17][1] )
-    y_rightmost = int(coordinates[18][1])
+    # y_leftmost = int(coordinates[2][1] )
+    # y_rightmost = int(coordinates[31][1])
     
     z_leftmost = int(coordinates[17][2])
     z_rightmost = int(coordinates[18][2])
     
 
-    cv2.circle(image, (x_leftmost, y_leftmost), radius=5, color=(255, 0, 0), thickness=-1)
-    cv2.circle(image, (x_rightmost, y_rightmost), radius=5, color=(255, 0, 0), thickness=-1)
+    cv2.circle(image, (coordinates[17][0], coordinates[17][1]), radius=5, color=(255, 0, 0), thickness=-1)
+    cv2.circle(image, (coordinates[18][0], coordinates[18][1]), radius=5, color=(255, 0, 0), thickness=-1)
+    cv2.circle(image, (coordinates[2][0], coordinates[2][1]), radius=5, color=(255, 0, 0), thickness=-1)
+    cv2.circle(image, (coordinates[31][0], coordinates[31][1]), radius=5, color=(255, 0, 0), thickness=-1)
+    
+    # plt.imshow(image)
+    # plt.show()
+    
 
 
-    print(x_leftmost, y_leftmost, z_leftmost)
-    print(x_rightmost, y_rightmost, z_rightmost)
+    # print(x_leftmost, y_leftmost, z_leftmost)
+
+    # print(x_rightmost, y_rightmost, z_rightmost)
    
 
     #Normalize the points based on the left most and the right most points
@@ -93,18 +111,82 @@ def normalized_coordinates(path):
     normalized_coordinates = []
     for point in coordinates:
         
-        x_norm = (point[0] -  x_leftmost) / (x_rightmost - x_leftmost)
-        y_norm = (point[1] -  y_leftmost) / (y_rightmost - y_leftmost)
+        x_norm = (point[0]) / (x_max)
+        y_norm = (point[1]) / (y_max)
+        # x_norm = point[0]
+        # y_norm = point[1]
+                       
 
         
-        normalized_coordinates.append([x_norm, y_norm])
+        normalized_coordinates.append([round(x_norm,4), round(y_norm,4)])
         
     return normalized_coordinates
 
+def change_path(path):
+    changed_path = path.replace('\\', '\\\\')
+    return changed_path
 
 
 if __name__=="__main__":
-    print(normalized_coordinates("C:\\Users\\schai\\OneDrive\\Desktop\\Course Project\\Photos\\Reddy\\IMG-20230618-WA0015-removebg-preview.png"))
+    folder_path = 'Photos\\background_remove'
+
+
+    file_paths = []
+
+    # Use os.walk() to traverse the folder and collect file paths
+    for root, directories, files in os.walk(folder_path):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            # file_path = change_path(file_path)
+            # print(file_path)
+            file_paths.append(file_path)
+            
+    
+    
+    # print(file_paths)
+    
+    remove_indices = [1, 3, 4, 6, 17,18, 19, 20, 21, 22, 30, 31]
+
+    model_name = []
+    coordinates_2d = []
+    for path in file_paths:
+        two_dim_coordinates = normalized_coordinates(path)
+        filtered_list = [value for index, value in enumerate(two_dim_coordinates) if index not in remove_indices]
+        # print(filtered_list[0])
+        model_name.append(list(path.split('\\'))[2].split('.')[0])
+        coordinates_2d.append(filtered_list)
+      
+        
+        
+    list_model_name = []
+    x_coordinates = []
+    y_coordinates = []
+    
+    for index in range(len(coordinates_2d)):
+        for pt in coordinates_2d[index]:
+            x_coordinates.append(pt[0])
+            y_coordinates.append(pt[1])
+            list_model_name.append(model_name[index])
+            
+    data = {
+        'model_name' : list_model_name, 
+        'x': x_coordinates, 
+        'y': y_coordinates
+    }
+    
+    df = pd.DataFrame(data)
+    df.to_csv('2d_dataset.csv')
+        
+    
+    
+    
+    print(len(coordinates_2d[0]))
+
+ 
+    # path = 'Photos\\Reddy\\IMG-20230618-WA0015-removebg-preview.png'
+    # two_dim = normalized_coordinates(path)
+    # print(len(two_dim))
+    # print(two_dim)
 
 
 
